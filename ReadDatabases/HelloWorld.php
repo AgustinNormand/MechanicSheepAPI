38736a8ca444903b542bc4369e86065d581c3272
Agustin <NormandAgustin@gmail.com>
2020-09-28 19:36:02 -0300

    First version of XBase reader

---
+++ ReadDatabases/HelloWorld.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Lectura Databases</title>
+    </head>
+    <body>
+        <p>Base de datos de VEHICULOS</p>
+
+        <?php
+
+        require "/home/agustin/Desktop/Seminario/HelloWorld/vendor/autoload.php";
+
+        use XBase\Table;
+
+        $table = new Table('/home/agustin/Desktop/Seminario/Databases/vehmae.dbf');
+
+        $records = $table->getRecordCount();
+        $columns = $table->getColumns();
+    
+        echo '<p>';
+        echo 'Cantidad de registros: ', $records;
+        echo '</p>';
+
+        echo '<p>';
+        echo 'Columnas:';
+        echo 'Patente, Apellido, Nombre, Marca, Modelo, AñoAuto, NumeroMotor, Vin, FechaCompra';
+        echo '</p>';
+
+        $patente = $columns['vehpat'];
+        $apellido = $columns['vehape'];
+        $nombre = $columns['vehnom'];
+        $marca = $columns['vehmar'];
+        $modelo = $columns['vehmod'];
+        $año = $columns['vehano'];
+        $numeroMotor = $columns['vehmot'];
+        $vin = $columns['vehvin'];
+        $fechaCompra = $columns['vehcom'];
+
+        echo '<p>';
+
+        while ($record = $table->nextRecord()) {
+            echo $record->$patente;
+            echo ', ';
+            echo $record->$apellido;
+            echo ', ';
+            echo $record->$nombre;
+            echo ', ';
+            echo $record->$marca;
+            echo ', ';
+            echo $record->$modelo;
+            echo ', ';
+            echo $record->$año;
+            echo ', ';
+            echo $record->$numeroMotor;
+            echo ', ';
+            echo $record->$vin;
+            echo ', ';
+            echo $record->$fechaCompra;
+            echo '<br>';
+
+        }
+        echo '</p>';
+        ?>
+        
+    </body>
+</html>
