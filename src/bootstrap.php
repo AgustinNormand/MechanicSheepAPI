d7106af35a08ae79b00adaf5b1799717bb9bddd8
Agustin <NormandAgustin@gmail.com>
2020-11-04 15:37:46 -0300

    Agrego codigo necesario para la conneccion a la db

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -4,6 +4,7 @@
     use Dotenv\Dotenv;
     use Monolog\Logger;
     use Monolog\Handler\StreamHandler;
+    use Illuminate\Database\Capsule\Manager as Capsule;
 
     use API\Core\Config;
 
@@ -17,3 +18,16 @@
     $handler->setLevel($config->get("LOG_LEVEL"));
     $log->pushHandler($handler);
 
+    $capsule = new Capsule;
+    $capsule->addConnection([
+        "driver" => $config->get("DB_CONNECTION"),
+        "host" => $config->get("DB_HOST"),
+        "port" => $config->get("DB_PORT"),
+        "database" => $config->get("DB_DATABASE"),
+        "username" => $config->get("DB_USERNAME"),
+        "password" => $config->get("DB_PASSWORD")
+     ]);
+     $capsule->setAsGlobal();
+     $capsule->bootEloquent();
+
+    
