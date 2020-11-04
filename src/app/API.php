f240db9002695eb4cce814f754396a9a507be94e
Agustin <NormandAgustin@gmail.com>
2020-11-04 15:30:45 -0300

    Arreglo errores en los limites de busqueda

--- src/app/API.php
+++ src/app/API.php
@@ -2,10 +2,11 @@
     require '..' . DIRECTORY_SEPARATOR .'bootstrap.php';
 
     //Borrar esto, es para developent nada mas
-    if ($handle = opendir("Z:\API\UntouchedDatabases")) 
+    $ds = DIRECTORY_SEPARATOR;
+    if ($handle = opendir(__DIR__.$ds.'..'.$ds.'..'.$ds."UntouchedDatabases")) 
         while (false !== ($entry = readdir($handle))) 
             if ($entry != "." && $entry != "..") 
-                copy("Z:\API\UntouchedDatabases\\" . $entry, 'C:/Users/Windows/Desktop/Sistema Mechanic Sheep/Core (CREO)/visual/OVEJA/' . $entry);
+                copy(__DIR__.$ds.'..'.$ds.'..'.$ds."UntouchedDatabases". $ds . $entry, 'C:/Users/Windows/Desktop/Sistema Mechanic Sheep/Core (CREO)/visual/OVEJA/' . $entry);
     closedir($handle);  
     //Borrar esto, es para developent nada mas
 
