690c10a58716050a743d56d04491c3fced31bafd
Agustin <NormandAgustin@gmail.com>
2020-11-09 18:00:03 -0300

    Elimino el archivo prueba

--- src/app/Prueba.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require '..' . DIRECTORY_SEPARATOR .'bootstrap.php';
-
-    use API\Core\Database\Tables\Table;
-    #use API\Core\Database\ColumnBuilders\ColumnBuilderClientes;
-
-$clientes = new Table(__DIR__."/../../DBS_FOR_TESTS/DBS_UNTOUCHED/climae.dbf", Table::CLIENTES);
-
-var_dump($clientes->getAllUndeletedRecords());
