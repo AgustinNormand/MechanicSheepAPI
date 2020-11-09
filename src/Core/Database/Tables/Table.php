1bdf0b5eb94617b27f1d2d55ad60e095a214ee39
Agustin <NormandAgustin@gmail.com>
2020-11-09 17:58:50 -0300

    Refactorizo al clase Table en funcion de las constantes con los nombres de las bases de datos

--- src/Core/Database/Tables/Table.php
+++ src/Core/Database/Tables/Table.php
@@ -4,15 +4,11 @@ namespace API\Core\Database\Tables;
 
 use XBase\Table as XbaseTable;
 use API\Core\Database\Records\Record;
+use API\Core\Config;
 #use XBase\Record\VisualFoxproRecord;
 
 class Table
 {
-    const CLIENTES = 'Clientes';
-    const DETALLES = 'Detalles';
-    const VEHICULOS = 'Vehiculos';
-    const TRABAJOS = 'Trabajos';
-
     private $fullPath;
 
     private $table;
@@ -23,19 +19,26 @@ class Table
 
     public function __construct($fullPath, $databaseName)
     {
-        $columnBuilder = "API\\Core\\Database\\ColumnBuilders\\ColumnBuilder{$databaseName}";
+        $this->fullPath = $fullPath; 
+        $columnBuilder = "API\\Core\\Enum\\DatabaseColumns\\DatabaseColumns{$databaseName}";
         $this->columns = $columnBuilder::$columns;
-        $this->fullPath = $fullPath;
-        $this->table = new XbaseTable($fullPath, $this->columns);
+        $this->table = new XbaseTable($this->fullPath, $this->columns);
         $this->recordCount = $this->table->getRecordCount();
+    }
+
+    public function getRecord($index){
+        $result = null;
+        $xbaseRecord = $this->table->pickRecord($index);
+        $result = $this->buildRecord($xbaseRecord);
+        return $result;
     }
 
     public function getUndeletedRecord($index)
     {
         $result = null;
-        $xbaseRecord = $this->table->pickRecord($index);
-        if(!$xbaseRecord->isDeleted())
-            $result = $this->buildRecord($xbaseRecord);
+        $record = $this->getRecord($index);
+        if(!$record->isDeleted())
+            $result = $record;
         return $result;
     }
 
@@ -53,6 +56,7 @@ class Table
     public function getUndeletedRecordsAfterIndex($index)
     {
         $result = [];
+        $index++; #Obtengo los registros que estén despues del indice dado.
         for($i=$index; $i < $this->recordCount; $i++){
             $xbaseRecord = $this->table->pickRecord($i);
             if(!$xbaseRecord->isDeleted())
@@ -76,23 +80,35 @@ class Table
         return $result;
     }
 
-    #public function isDeleted($record){}
+    public function getLastUndeletedRecordIndex()
+    {
+        $result = null;
+        $index = $this->recordCount - 1; #Obtengo el indice del último registro.
+        while ($index >= 0 and $result == null)
+        {
+            $xbaseRecord = $this->table->pickRecord($index);
+            if (!$xbaseRecord->isDeleted())
+                $result = $index;
+            $index--;
+        }
+        return $result;
+    }
 
-    #public function checkpoint()
-    #{
-    #    $fullPathOfBackupBase = $this->fullPath . ".bk";
-    #    copy($this->fullPath, $fullPathOfBackupBase);
-    #    return new Table($fullPathOfBackupBase, );
-    #}
+    public function getLastRecordIndex(){
+        return $this->recordCount - 1;
+    }
 
     public function isSameType($table)
     {
         return $this->table->getColumns() == $table->getColumns();
     }
 
-    #public function getLastRecordIndex(){}
+    public function getColumns()
+    {
+        return $this->table->getColumns();
+    }
 
-    #public function getLastUndeletedRecordIndex(){}
+    public function getUndeletedRecordCount(){}
 
     public function close()
     {
@@ -102,11 +118,12 @@ class Table
     private function buildRecord($xbaseRecord)
     {
         $data = [];
+        #$data[self::RECORD_INDEX_ATRIBUTE] = $xbaseRecord->getRecordIndex();
         foreach($this->columns as $column){
             $key = array_search($column, $this->columns);
             $data[$key] = $xbaseRecord->$column;
         }
-        return new Record($data);
+        return new Record($xbaseRecord->getRecordIndex(), $data, $xbaseRecord->isDeleted());
     }
 
 }
