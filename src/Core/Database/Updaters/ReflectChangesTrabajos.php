121fa6bbb40fc0a67e26fe82780dde5abc842eed
Agustin <NormandAgustin@gmail.com>
2020-11-25 14:27:01 -0300

    Elimino funciones que ahora se realizan en los modelos del ReflectChangesTrabajos

--- src/Core/Database/Updaters/ReflectChangesTrabajos.php
+++ src/Core/Database/Updaters/ReflectChangesTrabajos.php
@@ -24,97 +24,6 @@ class ReflectChangesTrabajos
         $this->columns = DatabaseColumnsTrabajos::$columns;
     }
 
-    /* Copiado del otro ReflectChanges, hay que refactorizar esto*/
-
-    private function obtenerOCrearMarca($marca){
-        if(strlen($marca) == 0)
-            $marca = "SinMarca";
-
-        $marca = Marca::firstOrCreate(
-            ['RAZON_SOCIAL' => $marca]
-        );
-        return $marca;
-    }
-
-    private function obtenerOCrearModelo($modelo, $marca){
-        if(strlen($modelo) == 0)
-            $modelo = "SinModelo";
-            $modelo = Modelo::firstOrCreate(
-            ['NOMBRE_FANTASIA' => $modelo],
-            ['ID_MARCA' => $marca->ID_MARCA]
-        );
-        return $modelo;
-    }
-
-    /* */
-
-    public function obtenerOSetearNuloServicio($descripcion){
-        if(strlen($descripcion) == 0)
-            $descripcion = "SinDescripcion";
-        $servicio = Servicio::firstOrCreate(["NOMBRE" => $descripcion]);
-        return $servicio;
-    }
-
-    private function crearVehiculo($record){
-        $reflectChangesVehiculo = new ReflectChangesVehiculos;
-
-        $marca = $reflectChangesVehiculo->obtenerOCrearMarca($record->get("MARCA"));
-
-        $modelo = $reflectChangesVehiculo->obtenerOCrearModelo($record->get("MODELO"), $marca);
-
-        $persona = $reflectChangesVehiculo->obtenerOSetearNuloPersona($record->get("NOMBRE"), $record->get("APELLIDO"));
-
-        $vehiculo = Vehiculo::create([
-            "PATENTE" => $record->get("PATENTE"), 
-            "ID_MODELO" => $modelo->ID_MODELO,
-            "ID_PERSONA" => $persona->ID_PERSONA
-        ]);
-
-        return $vehiculo;
-
-    }
-    
-    private function obtenerVehiculo($record){
-        $patenteAuto = $record->get("PATENTE");
-        $marca = $this->obtenerOCrearMarca($record->get("MARCA"));
-        $modelo = $this->obtenerOCrearModelo($record->get("MODELO"), $marca);
-        $vehiculo = Vehiculo::where([
-            ["PATENTE", $patenteAuto],
-            ["ID_MODELO", $modelo->ID_MODELO]
-            ])->get();
-            
-        if(count($vehiculo) == 0){
-            Log::error("Error in ReflectChangesTrabajos -> newRecords -> El select de vehiculo a la db, dio 0. Hay un trabajo realizado a un vehiculo no registrado.", [$record]);
-            $return = $this->crearVehiculo($record);
-            
-        } else
-            $return = $vehiculo[0];
-
-        if(count($vehiculo) > 1){
-            Log::error("Error in ReflectChangesTrabajos -> newRecords -> El select de vehiculo a la db, dio más que 1. Hay un trabajo realizado a un vehiculo que está duplicado ." , [$record]);
-        }
-        return $return;
-    }
-    /*
-    private function getAndSetIfDuplicated($numeroTrabajo){
-        $duplicated = 0;
-        if(!is_null($numeroTrabajo))
-        {
-            $trabajosDuplicados = Trabajo::where(
-                "NRO_TRABAJO", $numeroTrabajo
-                )->get();
-                if(count($trabajosDuplicados)>=1){
-                    $duplicated = 1;
-                    foreach($trabajosDuplicados as $trabajoDuplicado)
-                    {
-                        $trabajoDuplicado->DUPLICATED = 1;
-                        $trabajoDuplicado->save();
-                    }
-                }
-        }
-        return $duplicated;
-    }
-    */
     public function newRecords($records)
     {    
         foreach($records as $record)
@@ -149,7 +58,7 @@ class ReflectChangesTrabajos
                                                                       $trabajo->ID_TRABAJO,
                                                                       $record->get("NRO_SUCURSAL"));
             }catch(Exception $e){
-                Log::Error("Error in ReflectChangesTrabajos -> newRecords ->", [$e, $record]);
+                Log::Error("ReflectChangesTrabajos -> newRecords ->", [$e, $record]);
                 die;
             }
         }
@@ -164,7 +73,7 @@ class ReflectChangesTrabajos
                 $trabajo = Trabajo::find($record->getIndex());
                 $trabajo->delete();
             } catch(Exception $e){
-                Log::Error("Error in ReflectChangesTrabajos -> deletedRecords ->", [$e, $record]);
+                Log::Error("ReflectChangesTrabajos -> deletedRecords ->", [$e, $record]);
             }
         }
     }
@@ -186,7 +95,7 @@ class ReflectChangesTrabajos
                 }
                 $trabajo->save();
             } catch(Exception $e){
-                Log::Error("Error in ReflectChangesTrabajo -> modifiedRecords ->", [$e, $record]);
+                Log::Error("ReflectChangesTrabajo -> modifiedRecords ->", [$e, $record]);
             }
         }
     }
